183ce442106d44b9961a2ed1fba30835c9b2b003
hoanghuycao99 <hoanghuycao99@gmail.com>
2022-10-26 15:05:56 +0700

    second commit

--- js/script.js
+++ js/script.js
@@ -2,7 +2,13 @@ const h2React = React.createElement(
     'div',
     { className: 'div-id' },
     React.createElement('h2', { title: 'hoc' }, 'child1'),
-    React.createElement('p', null, 'child2')
+    React.createElement('p', null, 'child2'),
+    React.createElement(
+        'ul',
+        {},
+        React.createElement('li', {}, 'li1'),
+        React.createElement('li', {}, 'li2'),
+    )
 );
 
 const root = document.getElementById('root');
